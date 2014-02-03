78fe1d83f0be8a4bcd907fc9c8d153fa8e8a4c10
Pavithra <pksjce@gmail.com>
2014-02-03 19:51:21 +0530

    Add history functionality

--- main.js
+++ main.js
@@ -1,18 +1,57 @@
+
+var s = 2;
 $(document).ready(function(){
-	var console = $('#console');
-	console.keypress(function(evt){
+	var consoley = $('#console');
+	var historyStack = [];
+	var uparrowhit = 0;
+	consoley.keypress(function(evt){
 		if(evt.keyCode === 13){
-			var js = console.val();
-			var x = eval(js);
+			uparrowhit = 0;
+			var js = consoley.val();
+			var x;
+			var type = 'result';
+            var mockConsole = document.getElementById('myframe').contentWindow;
+			try{
+				x = mockConsole.eval(js);
+			}catch(e){
+				x = e.message;
+				type= 'error';
+			}
 			$('#userjs').html(js);
-			var ans = make_nice(x);
+			historyStack.push(js);
+			consoley.val('');
+			var ans = make_nice(x, type);
 			$('#answer').html(ans);
+		}
+	});
+	consoley.keydown(function(evt){
+		var len = historyStack.length;
+		if(evt.keyCode === 38 && len > 0){
+			uparrowhit +=1;
+			if(len >= uparrowhit){
+				consoley.val(historyStack[len - uparrowhit]);
+			}
+			evt.preventDefault();
+		} else if(evt.keyCode === 40 && len > 0 && uparrowhit > 0){
+			if(len >= uparrowhit && !evt.programmatic){
+				uparrowhit -=1;
+				consoley.val(historyStack[len - uparrowhit]);
+			} else {
+				return;
+			}
 		}
 	});
 });
 
-function make_nice(x){
-	if(x == undefined){
-		return "undefined";
+
+function make_nice(x, type){
+	if(type == "result"){
+		if(x === undefined){
+			return "undefined";
+		}	
+	} else{
+		return "Error: " + x;
 	}
+    return x;
+	
 }
